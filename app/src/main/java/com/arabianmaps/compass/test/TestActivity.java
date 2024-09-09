797fda28d95e73c7125a67ab2861c5db93fe529c
Sachin Rakibe <100836807+rakibesachin166@users.noreply.github.com>
2024-09-09 08:30:27 +0530

    design

---
+++ app/src/main/java/com/arabianmaps/compass/test/TestActivity.java
@@ -0,0 +1,16 @@
+package com.arabianmaps.compass.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.arabianmaps.compass.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sample_test);
+    }
+}
