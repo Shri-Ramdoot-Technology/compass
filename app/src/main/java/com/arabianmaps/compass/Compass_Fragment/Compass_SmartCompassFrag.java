450bf16377124ece9cef0316f6661573736ebb9d
Sachin Rakibe <100836807+rakibesachin166@users.noreply.github.com>
2024-09-11 07:54:16 +0530

    Compass Done

--- app/src/main/java/com/arabianmaps/compass/Compass_Fragment/Compass_SmartCompassFrag.java
+++ app/src/main/java/com/arabianmaps/compass/Compass_Fragment/Compass_SmartCompassFrag.java
@@ -21,11 +21,13 @@ import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
+
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -37,6 +39,7 @@ import com.google.android.gms.tasks.Task;
 import com.arabianmaps.compass.R;
 import com.arabianmaps.compass.Compass_Utils.Compass_C1105Ui;
 import com.arabianmaps.compass.Compass_Compass_customCompass.Compass_A;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
@@ -51,13 +54,13 @@ public class Compass_SmartCompassFrag extends Fragment {
     static double lng;
     public static TextView magneticHolder;
     String FinalLocationResult;
-    TextView addressHolder;
+    //    TextView addressHolder;
     List<Address> addressList;
     private float azimuth;
     private float azimuthFix;
     private Compass_A compass;
-    ImageView compassImg;
-    private float currentAzimuth;
+    ImageView compassImg, quibilaImg;
+    private float currentAzimuth, pastRotate = 0;
     TextView degreeDisplay;
     Geocoder geocoder;
     TextView latHolder;
@@ -67,9 +70,8 @@ public class Compass_SmartCompassFrag extends Fragment {
     FusedLocationProviderClient mFusedLocationClient;
     SensorManager magneticSensor;
     SensorManager sensorManager;
-   TextView speedTxt , altitudeTxt ,distanceToMTxt;
-    /* renamed from: v */
-    View f146v;
+    TextView speedTxt, altitudeTxt, distanceToMTxt;
+    /* renamed from: v */ View f146v;
     float degreeStart = 0.0f;
     private float[] mGravity = new float[3];
     private float[] mGeomagnetic = new float[3];
@@ -82,6 +84,7 @@ public class Compass_SmartCompassFrag extends Fragment {
     boolean isFirstInstall = true;
     final float alpha = 0.97f;
     Location meccaLocation = new Location("mecca");
+
 
     LocationCallback locationCallback = new LocationCallback() { // from class: com.digitalcompass.liveweather.Fragment.SmartCompassFrag.3
         @Override // com.google.android.gms.location.LocationCallback
@@ -107,10 +110,10 @@ public class Compass_SmartCompassFrag extends Fragment {
             flat = d;
             flng = d2;
             this.FinalLocationResult = str;
-            this.addressHolder = (TextView) getActivity().findViewById(R.id.addressHolder);
+//            this.addressHolder = (TextView) getActivity().findViewById(R.id.addressHolder);
             this.latHolder = (TextView) getActivity().findViewById(R.id.latHolder);
             this.lngHolder = (TextView) getActivity().findViewById(R.id.lngHolder);
-            this.addressHolder.setText(this.FinalLocationResult);
+//            this.addressHolder.setText(this.FinalLocationResult);
             this.latHolder.setText(doubToDMS(flat, true));
             this.lngHolder.setText(doubToDMS(flng, false));
             String str2 = TAG;
@@ -124,7 +127,7 @@ public class Compass_SmartCompassFrag extends Fragment {
     @Nullable
     public View onCreateView(@NonNull LayoutInflater layoutInflater, @Nullable ViewGroup viewGroup, @Nullable Bundle bundle) {
         this.f146v = layoutInflater.inflate(R.layout.d_layout_of_smart_fragment_layout, viewGroup, false);
-        Log.d("sachin","On create View ");
+        Log.d("sachin", "On create View ");
         setupCompass();
 //        RateThisApp.init(new RateThisApp.Config(5, 10));
 //        RateThisApp.onCreate(this.f146v.getContext());
@@ -145,8 +148,9 @@ public class Compass_SmartCompassFrag extends Fragment {
         this.degreeDisplay = (TextView) this.f146v.findViewById(R.id.degreeDisplay);
 
         this.compassImg = this.f146v.findViewById(R.id.compassImg);
+        this.quibilaImg = this.f146v.findViewById(R.id.quibilaImg);
 
-        LinearLayout linearLayout2 = this.f146v.findViewById(R.id.layAddressHolder);
+//        LinearLayout linearLayout2 = this.f146v.findViewById(R.id.layAddressHolder);
 
         ImageView imageView = (ImageView) this.f146v.findViewById(R.id.shareImg);
 
@@ -160,8 +164,8 @@ public class Compass_SmartCompassFrag extends Fragment {
                 Compass_SmartCompassFrag.this.startActivity(Intent.createChooser(intent, "Share Location"));
             }
         });
-        this.addressHolder = (TextView) this.f146v.findViewById(R.id.addressHolder);
-        this.addressHolder.setText(this.FinalLocationResult);
+//        this.addressHolder = (TextView) this.f146v.findViewById(R.id.addressHolder);
+//        this.addressHolder.setText(this.FinalLocationResult);
         this.latHolder.setText(doubToDMS(flat, true));
         this.lngHolder.setText(doubToDMS(flng, false));
         ProgressDialog progressDialog = new ProgressDialog(getContext());
@@ -174,7 +178,7 @@ public class Compass_SmartCompassFrag extends Fragment {
         progressDialog2.setCancelable(false);
         this.latHolder = this.f146v.findViewById(R.id.latHolder);
         this.lngHolder = this.f146v.findViewById(R.id.lngHolder);
-        Log.d("sachin","On create View ");
+        Log.d("sachin", "On create View ");
         getLastKnowLocation();
 //        Compass_C1105Ui.setHeightWidth(this.f146v.getContext(), this.latHolder, 216, 216);
 //        Compass_C1105Ui.setHeightWidth(this.f146v.getContext(), this.lngHolder, 216, 216);
@@ -196,7 +200,7 @@ public class Compass_SmartCompassFrag extends Fragment {
         this.addressList = this.geocoder.getFromLocation(d, d2, 1);
         List<Address> list = this.addressList;
         if (list != null) {
-            this.addressHolder.setText(list.get(0).getAddressLine(0));
+//            this.addressHolder.setText(list.get(0).getAddressLine(0));
             String str = TAG;
             Log.e(str, "Address is :" + this.addressList.get(0).getAddressLine(0));
             return this.addressList.get(0).getAddressLine(0);
@@ -210,13 +214,15 @@ public class Compass_SmartCompassFrag extends Fragment {
         googleLocationListener();
     }
 
+    private Location locationForRotate;
+
     @SuppressLint("MissingPermission")
-    private void googleLocationListener(){
-        Log.d("sachin","googleLocationListener");
+    private void googleLocationListener() {
+        Log.d("sachin", "googleLocationListener");
         FusedLocationProviderClient localFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext());
 
         // Configure LocationRequest
-        LocationRequest   locationRequest = LocationRequest.create();
+        LocationRequest locationRequest = LocationRequest.create();
         locationRequest.setInterval(5000); // 5 seconds
         locationRequest.setFastestInterval(5000); // 5 seconds
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
@@ -226,6 +232,8 @@ public class Compass_SmartCompassFrag extends Fragment {
             @Override
             public void onLocationResult(@NonNull LocationResult locationResult) {
                 for (Location localLocation : locationResult.getLocations()) {
+
+                    locationForRotate = localLocation;
                     // Do something with the location data
                     double latitude = localLocation.getLatitude();
                     double longitude = localLocation.getLongitude();
@@ -237,12 +245,12 @@ public class Compass_SmartCompassFrag extends Fragment {
 
                     float distanceInMeters = localLocation.distanceTo(meccaLocation);
 
-// Convert the distance to kilometers
                     float distanceInKilometers = distanceInMeters / 1000;
                     distanceToMTxt.setText(String.format("%.2f", distanceInKilometers));
                     Log.d("sachinGooLCAltitude", String.valueOf(localLocation.getAltitude()));
-                    Log.d("sachin",localLocation.toString());
+                    Log.d("sachin", localLocation.toString());
                     Log.d("sachinGooLCSpeed", String.valueOf(localLocation.getSpeed()));
+                    rotateQuibila();
                 }
             }
         };
@@ -250,6 +258,7 @@ public class Compass_SmartCompassFrag extends Fragment {
         localFusedLocationProviderClient.requestLocationUpdates(locationRequest, localLocationCallback, Looper.getMainLooper());
 
     }
+
     public boolean checkNetwork() {
 
         ConnectivityManager connectivityManager = (ConnectivityManager) getContext().getSystemService("connectivity");
@@ -258,16 +267,16 @@ public class Compass_SmartCompassFrag extends Fragment {
 
     @SuppressLint({"SetTextI18n", "MissingPermission"})
     public void getLastKnowLocation() {
-        Log.d("sachin","getLastKnowLocation()");
+        Log.d("sachin", "getLastKnowLocation()");
         if (checkPermission()) {
-            Log.d("sachin","Permission Granted()");
+            Log.d("sachin", "Permission Granted()");
             if (isLocationEnable()) {
-                Log.d("sachin","LocationEnabled calling fused set Client");
+                Log.d("sachin", "LocationEnabled calling fused set Client");
                 this.mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() { // from class: com.digitalcompass.liveweather.Fragment.SmartCompassFrag.2
                     @Override // com.google.android.gms.tasks.OnCompleteListener
                     @SuppressLint({"SetTextI18n"})
                     public void onComplete(@NonNull Task<Location> task) {
-                        Log.d("sachin","Location getted");
+                        Log.d("sachin", "Location getted");
                         Location result = task.getResult();
                         if (result != null) {
                             Compass_SmartCompassFrag.this.latHolder.setText(Compass_SmartCompassFrag.this.doubToDMS(result.getLatitude(), true));
@@ -275,12 +284,12 @@ public class Compass_SmartCompassFrag extends Fragment {
                             try {
                                 String addressList = Compass_SmartCompassFrag.this.getAddressList(result.getLatitude(), result.getLongitude());
                                 if (addressList != null) {
-                                    Compass_SmartCompassFrag.this.addressHolder.setText(addressList);
-                                    Compass_SmartCompassFrag.this.addressHolder.setEllipsize(TextUtils.TruncateAt.MARQUEE);
-                                    Compass_SmartCompassFrag.this.addressHolder.setMarqueeRepeatLimit(-1);
-                                    Compass_SmartCompassFrag.this.addressHolder.setFocusable(true);
-                                    Compass_SmartCompassFrag.this.addressHolder.setSelected(true);
-                                    Compass_SmartCompassFrag.this.addressHolder.setFocusableInTouchMode(true);
+//                                    Compass_SmartCompassFrag.this.addressHolder.setText(addressList);
+//                                    Compass_SmartCompassFrag.this.addressHolder.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+//                                    Compass_SmartCompassFrag.this.addressHolder.setMarqueeRepeatLimit(-1);
+//                                    Compass_SmartCompassFrag.this.addressHolder.setFocusable(true);
+//                                    Compass_SmartCompassFrag.this.addressHolder.setSelected(true);
+//                                    Compass_SmartCompassFrag.this.addressHolder.setFocusableInTouchMode(true);
                                 }
                             } catch (IOException e) {
                                 e.printStackTrace();
@@ -291,7 +300,7 @@ public class Compass_SmartCompassFrag extends Fragment {
                             Compass_SmartCompassFrag.this.lngHolder.setText(Compass_SmartCompassFrag.this.doubToDMS(Compass_SmartCompassFrag.lng, false));
                             return;
                         }
-                        Log.d("sachin","requestNewLocation");
+                        Log.d("sachin", "requestNewLocation");
                         Compass_SmartCompassFrag.this.requestNewLocation();
                     }
                 });
@@ -321,14 +330,14 @@ public class Compass_SmartCompassFrag extends Fragment {
 
     @SuppressLint("MissingPermission")
     public void requestNewLocation() {
-        Log.d("sachin","requestNewLocation()");
+        Log.d("sachin", "requestNewLocation()");
         LocationRequest locationRequest = new LocationRequest();
         locationRequest.setPriority(100);
         locationRequest.setInterval(5L);
         locationRequest.setFastestInterval(0L);
         this.mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this.f146v.getContext());
         checkPermission();
-        Log.d("sachin","called");
+        Log.d("sachin", "called");
         this.mFusedLocationClient.requestLocationUpdates(locationRequest, this.locationCallback, Looper.myLooper());
     }
 
@@ -430,12 +439,45 @@ public class Compass_SmartCompassFrag extends Fragment {
                 break;
 
         }
-        this.degreeDisplay.setText(firstDirection + "° " +"\n"+ secondDirection);
+        this.degreeDisplay.setText(firstDirection + "° " + "\n" + secondDirection);
+        rotateQuibila();
 //        this.degreeDisplay.setText(str2 + "° " + str);
         rotateAnimation.setDuration(500L);
         rotateAnimation.setRepeatCount(0);
         rotateAnimation.setFillAfter(true);
         this.compassImg.startAnimation(rotateAnimation);
+    }
+
+    //    private void rotateQuibila() {
+//        if (locationForRotate != null) {
+//            float bearingToPos = locationForRotate.bearingTo(meccaLocation); // Bearing in degrees
+//            float adjustedDirection = (bearingToPos - this.currentAzimuth + 360) % 360;
+//
+//            // Create and configure the rotation animation for the quibila
+//            RotateAnimation rotateAnimation = new RotateAnimation(-this.pastRotate, -adjustedDirection,
+//                    RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+//                    RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+//            this.pastRotate = adjustedDirection;
+//            rotateAnimation.setDuration(500L);
+//            rotateAnimation.setRepeatCount(0);
+//            rotateAnimation.setFillAfter(true);
+//            this.quibilaImg.startAnimation(rotateAnimation);
+//        }
+//    }
+    private void rotateQuibila() {
+        if (locationForRotate != null) {
+            // Calculate bearing from current location to Mecca
+            float bearingToMecca = locationForRotate.bearingTo(meccaLocation);
+
+            float adjustedDirection = (bearingToMecca - this.currentAzimuth + 360) % 360;
+
+            RotateAnimation rotateAnimation = new RotateAnimation(this.pastRotate, -adjustedDirection, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+            this.pastRotate = adjustedDirection;
+            rotateAnimation.setDuration(500L);
+            rotateAnimation.setRepeatCount(0);
+            rotateAnimation.setFillAfter(true);
+            this.quibilaImg.startAnimation(rotateAnimation);
+        }
     }
 
     public boolean isLocationEnable() {
